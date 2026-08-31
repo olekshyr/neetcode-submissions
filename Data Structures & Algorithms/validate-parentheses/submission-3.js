13cb17d7121b9a18a56a26e045cb16397be68eba
olekshyr <46656281+olekshyr@users.noreply.github.com>
2026-08-31 18:40:46 +0300

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        if (s.length % 2 > 0) return false;
+        const stack = [];
+        const pair = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+
+        for (let i = 0; i<s.length; i++) {
+            const char = s.charAt(i);
+            
+            if (char === '[' || char === '{' || char === '(') {
+                stack.push(char) 
+            } else if (pair[char] !== stack.pop()) {
+                return false;
+            } 
+        }
+
+        if (stack.length > 0) return false;
+
+        return true;
+    }
+}
