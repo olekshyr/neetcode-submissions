8640583e6980afcf1c174b24ebd312bbde908ac7
olekshyr <46656281+olekshyr@users.noreply.github.com>
2026-09-04 10:55:11 +0300

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string}
+     */
+    longestCommonPrefix(strs) {
+        let res = [];
+
+        for (let i = 0; i < strs[0].length; i++) {
+            let char = strs[0][i];
+
+            for (let j = 0; j < strs.length; j++) {
+                if (char != strs[j][i]) {
+                    return res.join('');
+                }
+            }
+
+            res.push(char);
+        }
+
+        return res.join('')
+    }
+}
